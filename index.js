6c9e50fcf22ed6ca4ed0f72ce751603a12ad182a
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2022-12-21 21:30:34 -0500

    commit tree c4b29814a0d908b3ba378dd834c480b27b5135bd

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat (name) {
+    cats.push(name);
+  };
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  };
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+  };
+
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+  };
+
+  function appendCat(name) {
+    const cats2 = [...cats]
+    cats2.push(name);
+    return cats2
+  };
+
+//   prependCat(name)
+//   1) prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+  
+  function prependCat(name) {
+    const cats2 = [...cats]
+    cats2.unshift(name);
+    return cats2
+  };
+
+
+//   removeLastCat()
+//   1) removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+
+function removeLastCat(name) {
+    const cats2 = [...cats]
+    cats2.pop(name);
+    return cats2
+  };
+
+  function removeFirstCat(name) {
+    const cats2 = [...cats]
+    cats2.shift(name);
+    return cats2
+  };
